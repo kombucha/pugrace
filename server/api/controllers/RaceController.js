e4a2efb332bc0555b4e2e8cba18b82d9e869927f
kombucha <vincent.lemeunier+github@gmail.com>
2014-10-05 09:10:55 +0000

    WIP

--- server/api/controllers/RaceController.js
+++ server/api/controllers/RaceController.js
@@ -6,6 +6,28 @@
  */
 
 module.exports = {
+    join: function(req, res) {
+        var pugId = 1, // TODO: get it from session
+            raceId = req.param('id');
 
+            Race.findOne(raceId).populate('runs').then(function(race) {
+                // TODO: put this in a policy
+                // if (!race.state === 'waiting') {
+                //     return res.error('Can\'t join the race');
+                // }
+
+                return Run.create({}).then(function(run) {
+                    race.runs.add(run);
+                    return race.save();
+                });
+            })
+            .then(function (race) {
+                return res.json(race);
+            })
+            .catch(function(err) {
+                sails.log.error(err);
+                return res.serverError('Failed to join race');
+            });
+    }
 };
 
--- server/config/routes.js
+++ server/config/routes.js
@@ -21,5 +21,5 @@
  */
 
 module.exports.routes = {
-
+    'POST /api/race/:id/join': 'RaceController.join'
 };
